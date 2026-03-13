63566af0f90eeb4d99e8f07f1c0368048cd5b745
kodytekjiri24 <kodytekjiri24@spseplzen.cz>
2026-03-13 16:26:55 +0100

    funguje lobby

--- script_mistnost.js
+++ script_mistnost.js
@@ -3,5 +3,6 @@ const obrazy = document.querySelectorAll(".obrazy");
 obrazy.forEach(el => {
   el.addEventListener("click", () => {
     window.location.href = "editace.html"; // otevře jiný index/html
+
   });
 });
