d8698a17c08776f3ed880b9cfb954c09f2ac1179
anhx <terezaanhhoang@gmail.com>
2026-03-13 16:06:39 +0100

    fungující button

--- button.js
+++ button.js
@@ -1,6 +1,17 @@
 const btn = document.getElementById("enterBtn");
 const landing = document.getElementById("landing");
 
-btn.addEventListener("click", () => {
-  landing.classList.add("slide-up");
-});
+if (btn) {
+  btn.addEventListener("click", () => {
+    if (landing) {
+      landing.classList.add("slide-up");
+      setTimeout(() => {
+        window.location.href = "index_mistnost.html";
+      }, 800);
+      return;
+    }
+
+    window.location.href = "index_mistnost.html";
+  });
+}
+
